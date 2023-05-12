49ef543ff590ad7b2ee2baa348f794bbd5eee894
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-05-12 20:38:52 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log("qwe");
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+const createItem = ({preview,original,description}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`;
+
+
+
+const createGallery = galleryItems.map(createItem).join("");
+
+gallery.insertAdjacentHTML("beforeend",createGallery);
+console.log(galleryItems);
+
+
+
+
+const lightbox  = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,  
+  });
+console.log(lightbox);
