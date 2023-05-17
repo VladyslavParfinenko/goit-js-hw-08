293d48780316646d9b486b5f06a6d2c5881e0f20
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-05-17 17:30:44 +0300

    dz 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+     const currentTime = throttle(function(data)  {
+        const currentTime = data.seconds;
+        localStorage.setItem('videoplayer-current-time', currentTime);
+    },1000);
+    
+   
+
+
+    player.on('timeupdate',currentTime);
+
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    player.setCurrentTime(savedTime);
+
+    
